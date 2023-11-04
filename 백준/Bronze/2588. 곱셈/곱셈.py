ad1505a53f3bce3c0b9c4849e7ec2ce2f6293601
Boong <81848426+boongseop123@users.noreply.github.com>
2023-11-04 11:54:49 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,9 +1,8 @@
-a=int(input())
-b=input()
-print(a*int(b[2]))#a*(b%10)
-print(a*int(b[1]))#a*((b%100)//10)
-print(a*int(b[0]))#a*(b%100)
-print(a*int(b))
+A = int(input())
+B = int(input())
 
-#a,b를 둘다 실수로 받은 후
-#나머지 연산과 몫연산을 이용해서 풀 수도 있다.
+print(A * (B % 10))
+print(A * (B % 100 // 10))
+print(A * (B // 100))
+print(A * B)
+
